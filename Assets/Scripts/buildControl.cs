5f346f2a1d77dc9769121b55611136d52250a6b6
doidexx <doidexx000@gmail.com>
2018-11-28 16:49:48 -0500

    creating first level

---
+++ Assets/Scripts/buildControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildControl : MonoBehaviour {
+
+    //buildint Time
+    private float BT;
+    //Press E phisical object
+    public GameObject pE;
+	// Use this for initialization
+	void Start () {
+        BT = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(BT);
+	}
+
+    void OnTriggerStay(Collider other) {
+        if (Input.GetKey(KeyCode.E)) {
+            if (other.CompareTag("player")) {
+                pE.SetActive(true);
+                if (BT < 8) {
+                    BT += Time.fixedDeltaTime;
+                }
+            } else {
+                pE.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/playerS.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerS : MonoBehaviour {
+    Rigidbody pbody;
+    private float speed;
+	// Use this for initialization
+	void Start () {
+        pbody = GetComponent<Rigidbody>();
+        speed = 15f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.W)) {
+            pbody.MovePosition(transform.position + Vector3.forward * Time.fixedDeltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            pbody.MovePosition(transform.position + Vector3.left * Time.fixedDeltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            pbody.MovePosition(transform.position + Vector3.right * Time.fixedDeltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            pbody.MovePosition(transform.position + Vector3.back * Time.fixedDeltaTime * speed);
+        }
+    }
+}
